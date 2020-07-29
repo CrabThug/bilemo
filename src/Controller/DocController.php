765e6e737f5a05b8ac4252df29d58a17201564e9
CrabThug <damien.millet@nasm.fr>
2020-07-29 12:09:29 +0200

    :art: Improving structure / format of the code. path ("/") redirect now to the route ("/api/docs",name="docs")

--- src/Controller/DocController.php
+++ src/Controller/DocController.php
@@ -1,6 +1,4 @@
 <?php
-
-
 namespace App\Controller;
 
 
@@ -8,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class DocController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
